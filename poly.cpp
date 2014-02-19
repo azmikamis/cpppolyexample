bf8dd62a980a63aad42e3e42742db474bf919ce8
azmikamis <azmi.stuff@gmail.com>
2014-02-19 14:29:03 +0800

    first commit

--- poly.cpp
+++ poly.cpp
@@ -9,10 +9,10 @@ int main () {
 
   Animal *Ourhouse[4];
 
-  Ourhouse[0] = new Human((char *)"Brenda",(char *)"Fortesque",21);
-  Ourhouse[1] = new Human((char *)"Bob",(char *)"Marley",52);
-  Ourhouse[2] = new Pet((char *)"Robin",6,(char *)"Dog",29);
-  Ourhouse[3] = new Pet((char *)"Tigger",10,(char *)"Cat",50);
+  Ourhouse[0] = new Human((char *)"Brenda", (char *)"Fortesque", 21);
+  Ourhouse[1] = new Human((char *)"Bob", (char *)"Marley", 52);
+  Ourhouse[2] = new Pet((char *)"Robin", 6, (char *)"Dog", 29);
+  Ourhouse[3] = new Pet((char *)"Tigger", 10, (char *)"Cat", 50);
 
   int k;
   for (k=0; k<4; k++) {
